6069a1a4dd2d2742f71f251cc94606bea6e774a2
CamHickling <camhickling@gmail.com>
2022-10-17 05:49:59 -0700

    fixed more issues. simplified card fields.

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -5,15 +5,22 @@ import java.util.List;
 
 // Card represents a playing card from a deck
 public class Card {
-    private static final List<String> DECK = new ArrayList<String>(
-            List.of("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"));
-
+    private static List<String> DECK;
 
     private final String name;
     private final int value;
 
     //EFFECTS: instantiates a card instance, assigns a random name from deck and the appropriate card value
     public Card() {
+        DECK = new ArrayList<String>();
+        for (int i = 2; i <= 10; i++) {
+            DECK.add(String.valueOf(i));
+        }
+        DECK.add("J");
+        DECK.add("Q");
+        DECK.add("K");
+        DECK.add("A");
+
         this.name = DECK.get((int) (Math.random() * DECK.size()));
         if (this.name == "J" || this.name == "Q" || this.name == "K") {
             this.value = 10;
